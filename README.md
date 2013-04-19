ea59122c534ff26554ff12ac3e6f2c837b68dd80
xuchdong <xuchdong@gmail.com>
2013-04-19 15:17:40 +0800

    finished: v0.1

--- README.md
+++ README.md
@@ -2,3 +2,25 @@ godouban
 ========
 
 Douban go's SDK
+
+使用说明:
+    
+    package main
+    
+    import (
+        "fmt"
+        "douban"
+    )
+    
+    func main(){
+        client := douban.Client{
+        }
+
+        url_, _ := client.GetAuthUrl()
+        fmt.Printf("Go to the following link in your browser:\n %s\n", url_)
+
+        code := new([]byte)
+        fmt.Scanf("Enter the verification code:%s", code)
+        tok, _= client.GetAccessToken(string(*code))
+    }
+
--- client.go
+++ client.go
@@ -1,84 +1,102 @@
 package douban
 
 import (
-    "net/http"
+    "time"
     "net/url"
-    "errors"
+    "net/http"
     "io/ioutil"
-    "fmt"
+    "encoding/json"
 )
+
+const (
+    ApiHost         string = "https://api.douban.com"
+    AuthHost        string = "https://www.douban.com"
+    TokenUrl        string = AuthHost + "/service/auth2/token"
+    AuthorizeUrl    string = AuthHost + "/service/auth2/auth"
+)
+
+type Token struct {
+    AccessToken     string
+    RefreshToken    string
+    Expiry          time.Time
+}
+
+func (t *Token) Expired() bool {
+    if t.Expiry.IsZero() {
+        return false
+    }
+    return t.Expiry.Before(time.Now())
+}
+
 
 type Client struct {
     ClientID        string
     ClientSecret    string
     CallBack        string
-    AccessToken     string
-    User            string
-    UserId          string
-    RefreshToken    string
 }
-
 
 func (c *Client) GetAuthUrl() (string, error) {
-    auth_url := "https://www.douban.com/service/auth2/auth"
-    r, err := url.Parse(auth_url)
+    url_, err := url.Parse(AuthorizeUrl)
     if err != nil {
-        return "", errors.New("parse url errror")
+        return "", err
     }
 
-    p := url.Values{}
-    p.Set("client_id", c.ClientID)
-    p.Set("redirect_uri", c.CallBack)
-    p.Set("response_type", "code")
-    // Don't set scope, state
-    r.RawQuery = p.Encode()
+    q := url.Values{
+        "client_id":        {c.ClientID},
+        "redirect_uri":     {c.CallBack},
+        "response_type":    {"code"},
+    }.Encode()
+    if url_.RawQuery == "" {
+        url_.RawQuery = q
+    } else {
+        url_.RawQuery += "&" + q
+    }
 
-    return r.String(), nil
+    return url_.String(), nil
 }
 
-func (c *Client) GetAccessToken(code string) error {
-    r := "https://www.douban.com/service/auth2/token"
-    v := url.Values{}
-    v.Set("client_id", c.ClientID)
-    v.Set("client_secret", c.ClientSecret)
-    v.Set("redirect_uri", c.CallBack)
-    v.Set("grant_type", "authorization_code")
-    v.Set("code", code)
-    resp, err := http.PostForm(r, v)
-    if err != nil {
-        return errors.New("post error")
+func (c *Client) GetAccessToken(code string) (*Token, error) {
+    v := url.Values{
+        "client_id":        {c.ClientID},
+        "client_secret":    {c.ClientSecret},
+        "redirect_uri":     {c.CallBack},
+        "grant_type":       {"authorization_code"},
+        "code":             {code},
     }
+
+    resp, err := http.PostForm(TokenUrl, v)
+    if err != nil {
+        return nil, err
+    }
+
     body, err := ioutil.ReadAll(resp.Body)
     if err != nil {
-        return errors.New("read error")
+        return nil, err
     }
-    fmt.Printf("%s\n", body)
-    return nil
+
+    var m struct {
+        Access      string          `json:"access_token"`
+        Refresh     string          `json:"refresh_token"`
+        ExpiresIn   time.Duration   `json:"expires_in"`
+    }
+
+    if err := json.Unmarshal(body, &m); err != nil {
+        return nil, err
+    }
+
+    m.ExpiresIn *= time.Second
+
+    var tok Token
+    tok.AccessToken = m.Access
+    if len(m.Refresh) > 0 {
+        tok.RefreshToken = m.Refresh
+    }
+    if m.ExpiresIn == 0 {
+        tok.Expiry = time.Time{}
+    } else {
+        tok.Expiry = time.Now().Add(m.ExpiresIn)
+    }
+
+    return &tok, nil
 }
-
-func (c *Client) GetUserInfo() error {
-    rs := "https://api.douban.com/v2/user/:" + c.User
-    r, err := url.Parse(rs)
-    if err != nil {
-        return errors.New("parse error")
-    }
-
-    v := url.Values{}
-    v.Set("access_token", c.AccessToken)
-    v.Set("apikey", c.ClientID)
-    r.RawQuery = v.Encode()
-
-    fmt.Printf(r.String())
-    resp, err := http.Get(r.String())
-    if err != nil {
-        return errors.New("get error")
-    }
-    body, err := ioutil.ReadAll(resp.Body)
-    if err != nil {
-        return errors.New("read error")
-    }
-    fmt.Printf("%s\n", body)
-    return nil
-}
-
 
